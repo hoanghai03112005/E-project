04685c7bd9a83457a61d4c3656fe5ef04a1a763a
sapogcacvaza <vuthanhlong909@gmail.com>
2024-09-19 22:12:56 +0700

    long day App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Footer from './components/footer';
 import Feedback from './components/feedback/feedback';
 
 import Contact from './components/contact_us/contact';
+import Shop_2 from './components/shop/shop_2';
 
 
 
@@ -19,18 +20,19 @@ function App() {
   return (
     <div className="App">
 
-        <Navbar />
-        <Routes>
-            <Route path='/' element={<HomeMain />} />
-            <Route path='/shop' element={<ShopMain />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/shop-detail/:id' element={<ShopDetail />} />
-            <Route path='/search/' element={<Search />} />
-            <Route path='/feedback' element={<Feedback />} />
-            <Route path='/contact' element={<Contact />} />
-        </Routes>
-        <Footer />
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomeMain />} />
+        <Route path='/shop' element={<ShopMain />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/design-style' element={<Shop_2 />} />
+        <Route path='/shop-detail/:id' element={<ShopDetail />} />
+        <Route path='/search/' element={<Search />} />
+        <Route path='/feedback' element={<Feedback />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+
 
     </div>
   );
