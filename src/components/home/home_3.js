2584ecffdb18723fe844ebc5e91469017fceb376
sapogcacvaza <vuthanhlong909@gmail.com>
2024-09-19 22:50:29 +0700

    long add home2

--- src/components/home/home_3.js
+++ src/components/home/home_3.js
@@ -6,8 +6,8 @@ export default function Home_3() { // Featurs
                     <div class="row g-4 justify-content-center">
                         <h1>Our Services</h1>
                         <div class="col-md-6 col-lg-4">
-                            <a href="#">
-                                <div class="service-item bg-secondary border-3 border-secondary border-dark">
+                            <a href="/design-style">
+                                <div class="service-item bg-white border-3 border-secondary border-dark">
                                     <img src="img/homeImage/bookcase1.jpg" class="img-fluid w-100" alt="" />
                                     <div class="px-4 rounded-bottom">
                                         <div class="service-content bg-dark text-center p-4 rounded">
@@ -19,8 +19,8 @@ export default function Home_3() { // Featurs
                             </a>
                         </div>
                         <div class="col-md-6 col-lg-4">
-                            <a href="#">
-                                <div class="service-item bg-secondary border-3 border-secondary border-dark">
+                            <a href="/design-style">
+                                <div class="service-item bg-white border-3 border-secondary border-dark">
                                     <img src="img/homeImage/sofa2.jpg" class="img-fluid w-100" alt="" />
                                     <div class="px-4 rounded-bottom">
                                         <div class="service-content bg-dark text-center p-4 rounded">
@@ -32,8 +32,8 @@ export default function Home_3() { // Featurs
                             </a>
                         </div>
                         <div class="col-md-6 col-lg-4">
-                            <a href="#">
-                                <div class="service-item bg-secondary border-3 border-secondary border-dark">
+                            <a href="/design-style">
+                                <div class="service-item bg-white border-3 border-secondary border-dark">
                                     <img src="img/homeImage/sofa3.jpg" class="img-fluid w-100" alt="" />
                                     <div class="px-4 rounded-bottom">
                                         <div class="service-content bg-dark text-center p-4 rounded">
