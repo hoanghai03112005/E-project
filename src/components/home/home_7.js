458d90afc16da2595fce83766986ca0517618d21
sapogcacvaza <vuthanhlong909@gmail.com>
2024-09-21 06:15:49 +0700

    dafldf

--- src/components/home/home_7.js
+++ src/components/home/home_7.js
@@ -6,77 +6,74 @@ export default function Home_7() { //Fact
                     <div class="row g-5 align-items-center">
                         <h1>Our Products</h1>
                         <div class="col-md-12 col-lg-3">
-                            <img src="img/homeImage/sofa1.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
+                            <img src="img/product/chair2.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
                             <div class="p-4 rounded-bottom">
-                                <h4>Parsely</h4>
+                                <h4>Armchair Doulton vintage</h4>
                                 <p class="text-truncate">Lorem ipsum dolor sit amet consectetur</p>
-                                <div class="d-flex justify-content-between flex-lg-wrap">
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
+                                <div class="d-flex justify-content-center flex-lg-wrap">
+                                    <p class="text-dark fs-5 fw-bold mb-0">$1200</p>
                                 </div>
                                 <div className="row">
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="fa fa-shopping-bag me-2 text-white"></i>Add</a>
+                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-cart-fill"></i>Add</a>
                                     </div>
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white  fs-6"><i class="bi bi-eye-fill me-2 text-white"></i>View</a>
+                                        <a href="/shop" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-eye-fill"></i>View</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12 col-lg-3">
-                            <img src="img/homeImage/sofa1.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
+                            <img src="https://cdn.thewirecutter.com/wp-content/media/2022/01/bedframes-2048px-floyd-1.jpg?auto=webp&quality=75&width=1024" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
                             <div class="p-4 rounded-bottom">
-                                <h4>Parsely</h4>
+                                <h4>Contemporary Bed Frame</h4>
                                 <p class="text-truncate">Lorem ipsum dolor sit amet consectetur</p>
-                                <div class="d-flex justify-content-between flex-lg-wrap">
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
+                                <div class="d-flex justify-content-center flex-lg-wrap">
+                                    <p class="text-dark fs-5 fw-bold mb-0">$25000</p>
                                 </div>
                                 <div className="row">
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="fa fa-shopping-bag me-2 text-white"></i>Add</a>
+                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-cart-fill"></i>Add</a>
                                     </div>
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white  fs-6"><i class="bi bi-eye-fill me-2 text-white"></i>View</a>
+                                        <a href="/shop" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-eye-fill"></i>View</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12 col-lg-3">
-                            <img src="img/homeImage/sofa1.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
+                            <img src="https://www.thekitchenpartners.co.uk/wp-content/uploads/2023/11/Hanna-1-scaled.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
                             <div class="p-4 rounded-bottom">
-                                <h4>Parsely</h4>
+                                <h4>Versatile Kitchen Island</h4>
                                 <p class="text-truncate">Lorem ipsum dolor sit amet consectetur</p>
-                                <div class="d-flex justify-content-between flex-lg-wrap">
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
+                                <div class="d-flex justify-content-center flex-lg-wrap">
+                                    <p class="text-dark fs-5 fw-bold mb-0">$1200</p>
                                 </div>
                                 <div className="row">
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="fa fa-shopping-bag me-2 text-white"></i>Add</a>
+                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-cart-fill"></i>Add</a>
                                     </div>
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white  fs-6"><i class="bi bi-eye-fill me-2 text-white"></i>View</a>
+                                        <a href="/shop" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-eye-fill"></i>View</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12 col-lg-3">
-                            <img src="img/homeImage/sofa1.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
+                            <img src="img/product/sofa1.jpg" class="img w-100 h-150 bg-secondary rounded" alt="First slide" />
                             <div class="p-4 rounded-bottom">
-                                <h4>Parsely</h4>
+                                <h4>Hung King 3-seater Sofa + Pillow</h4>
                                 <p class="text-truncate">Lorem ipsum dolor sit amet consectetur</p>
-                                <div class="d-flex justify-content-between flex-lg-wrap">
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                    <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
+                                <div class="d-flex justify-content-center flex-lg-wrap">
+                                    <p class="text-dark fs-5 fw-bold mb-0">$1200</p>
                                 </div>
                                 <div className="row">
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="fa fa-shopping-bag me-2 text-white"></i>Add</a>
+                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-cart-fill"></i>Add</a>
                                     </div>
                                     <div className="col-lg-6">
-                                        <a href="#" class="btn border border-dark bg-dark rounded px-3 text-white  fs-6"><i class="bi bi-eye-fill me-2 text-white"></i>View</a>
+
+                                        <a href="/shop" class="btn border border-dark bg-dark rounded px-3 text-white fs-6"><i class="bi bi-eye-fill"></i>View</a>
                                     </div>
                                 </div>
                             </div>
